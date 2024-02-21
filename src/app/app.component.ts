63fcae68e135240b47d731780f64d28a45309ee9
unknown <vaibhavnarsinge>
2024-02-21 18:00:12 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FirstAppp';
+  data:any;
+  dataa:any="This is from Parent";
+ 
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+export class LoginComponent {
+  @Input() item = '';
+show="amol";
+color="blue";
+list:string[]= ["Rohit","Sagar","Amol"];
+
+users=[
+  {name:'amol',age:23, email:'amol@gmail.com',account:['facebook','Twiter','linkedin']},
+  {name:'rahul',age:21, email:'rahul@gmail.com',account:['facebook','Twiter','linkedin']},
+  {name:'sagar',age:22, email:'sagar@gmail.com',account:['facebook','Twiter','linkedin']},
+
+]
+name="amol";
+disable = false;
+data:any;
+
+getcolor(colorr:any){
+  this.color = colorr;
+}
+
+gettitle(){
+  alert("this is button alert")
+}
+
+getName(name:any){
+  alert(name);
+  console.log(name);
+}
+
+getname(name:any){
+  console.log(name);
+}
+
+getname1(name:any){
+  console.log(name);
+}
+getname2(name:any){
+  console.log(name);
+}
+
+}
